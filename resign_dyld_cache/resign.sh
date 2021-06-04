bf722239920ea45e79e4f1c18a9de9f2c4da1ef5
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-06-03 20:46:12 -0400

    resign_dyld_cache: compute CDHash for Taurine

--- resign_dyld_cache/resign.sh
+++ resign_dyld_cache/resign.sh
@@ -6,3 +6,7 @@ python3 remove_signature.py "$1"
 	--timestamp=none \
 	"$1"
 python3 fixup_signature_size.py "$1"
+target_path="/usr/local/zhuowei/dyld_shared_cache_arm64e"
+sha256_filename="$(echo -n "$target_path" | shasum -a 256 | cut -f 1 -d " ")"
+codesign -d -vvv "$1" 2>&1 |grep "^CDHash="|cut -f 2 -d "="|xxd -r -p >"$sha256_filename"
+echo "put $sha256_filename in /taurine/cstmp/"
